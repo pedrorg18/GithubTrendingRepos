af85dc176db93f901c82e3b594cc036e104a8293
Pedro Roig <pedro.roiggimenez@soprasteria.com>
2020-05-14 17:45:25 +0200

    android test: create test runner that will use the TestApplication class, and also will force the io Scheduler to finish synchronously

---
+++ mobile-ui/src/androidTest/java/com/pedroroig/mobile_ui/test/TestRunner.kt
@@ -0,0 +1,27 @@
+package com.pedroroig.mobile_ui.test
+
+import android.app.Application
+import android.content.Context
+import android.os.Bundle
+import android.support.test.runner.AndroidJUnitRunner
+import io.reactivex.rxjava3.plugins.RxJavaPlugins
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+class TestRunner : AndroidJUnitRunner() {
+
+    override fun onCreate(arguments: Bundle?) {
+        super.onCreate(arguments)
+        RxJavaPlugins.setInitIoSchedulerHandler {
+            Schedulers.trampoline()
+        }
+    }
+
+    override fun newApplication(
+        cl: ClassLoader,
+        className: String,
+        context: Context
+    ): Application {
+        return super.newApplication(cl, TestApplication::class.java.name, context)
+
+    }
+}
